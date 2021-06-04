23e0d6c59f32e4e5c4211e71010f40a9fb0be3e0
20402김재현 <59628836+gomdols@users.noreply.github.com>
2021-06-04 20:43:02 +0900

    new model "User"

--- post/models.py
+++ post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model) :
+    user_name = models.CharField(max_length=16)
+    user_email = models.CharField(max_length=60)
+    user_password = models.CharField(max_length=256)
+    user_introduction = models.TextField(max_length=600)
+    user_create_date = models.DateField(auto_now=False, auto_now_add=True)
+    user_point = models.IntegerField(default=0)
