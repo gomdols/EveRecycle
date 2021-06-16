29242a930144ea45615504ecef7116fea1a77de1
20402김재현 <59628836+gomdols@users.noreply.github.com>
2021-06-17 02:29:19 +0900

    restart-bro

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,38 +1,24 @@
 module.exports = {
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: false,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(ttf|eot|woff|woff2|svg)$/,
-                use: {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'src/components/fonts/'
-                    },
-                },
-            },
-        ],
-    },
-};
+      rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            use: [
+              'url-loader?limit=10000',
+              'img-loader'
+            ]
+        }
+      ]
+    }
+  };
