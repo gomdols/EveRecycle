ef8749a8a9e07d8e4e32f47ea5566ac117c68c7a
gomdols <jininv@naver.com>
2021-06-17 10:36:59 +0900

    index.html

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,24 +1,24 @@
 module.exports = {
     module: {
-      rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: [
+                'url-loader?limit=10000',
+                'img-loader'
+                ]
             }
-        },
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            use: [
-              'url-loader?limit=10000',
-              'img-loader'
-            ]
-        }
-      ]
+        ]
     }
-  };
+};
